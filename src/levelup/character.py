017a75b8d0b9ab32a0574824546163dc4b1bda1f
Jason K <jason.h.knight@gmail.com>
2023-03-31 18:26:30 +0000

    updated character, test_character, and map

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -17,7 +17,8 @@ class Character:
         self.name = name or DEFAULT_CHARACTER_NAME
 
     def enter_map(self, game_map: GameMap):
-        pass
+        self.map = game_map
+        self.position = self.map.starting_position
 
     def move(self, direction: Direction):
         next_position = self.map.calculate_position(self.position, direction)
